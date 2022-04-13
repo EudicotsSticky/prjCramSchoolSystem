4b78baba3b38ab9aa05636fe7c3674fb2c99c103
EudicotsSticky <ratvincent0626@gmail.com>
2022-04-13 19:30:27 +0800

    加入專案檔案。

---
+++ prjCramSchoolSystem/Models/TStudentProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace prjCramSchoolSystem.Models
+{
+    public partial class TStudentProfile
+    {
+        public int FId { get; set; }
+        public string FAccount { get; set; }
+        public string FPassword { get; set; }
+        public string FName { get; set; }
+        public string FGender { get; set; }
+        public DateTime? FBirthDate { get; set; }
+        public string FPhone { get; set; }
+        public string FEmail { get; set; }
+        public string FAddress { get; set; }
+        public string FEnrollment { get; set; }
+        public string FGrade { get; set; }
+        public string FStatus { get; set; }
+        public string FThumbnailUrl { get; set; }
+        public string FParentName { get; set; }
+        public DateTime? FCreateDate { get; set; }
+        public DateTime? FUpdateDate { get; set; }
+    }
+}
